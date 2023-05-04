955f6d46a9d727ab418c9d42e09a5616dfeba311
iwalkalone <iwalkalone69@gmail.com>
2023-05-04 11:16:16 +0200

    Fix
    Signed-off-by: iwalkalone <iwalkalone69@gmail.com>

--- src/irc.ts
+++ src/irc.ts
@@ -619,8 +619,9 @@ export class Client extends (EventEmitter as unknown as new () => TypedEmitter<C
 
         // finding what channels a user is in
         this.state.chans.forEach((nickChannel, channame) => {
-            if (message.nick && nickChannel.users.has(message.nick)) {
-                nickChannel.users.set(message.args[0], nickChannel.users.get(message.nick));
+            const chanUser = message.nick && nickChannel.users.get(message.nick);
+            if (message.nick && chanUser) {
+                nickChannel.users.set(message.args[0], chanUser);
                 nickChannel.users.delete(message.nick);
                 channelsForNick.push(channame);
             }
